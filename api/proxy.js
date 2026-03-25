e91736c7fc7cab84fb00db2364ec5dc02fe6ed3a
easytypeneocitiesorg-lgtm <easytype.neocities.org@gmail.com>
2026-03-25 16:13:50 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,11 @@
+import { createBareServer } from "@titaniumnetwork-dev/ultraviolet";
+
+const bare = createBareServer("/bare/");
+
 export default async function handler(req, res) {
-  const { url } = req.query;
-
-  if (!url) {
-    return res.status(400).send("Missing URL");
+  if (bare.shouldRoute(req)) {
+    return bare.routeRequest(req, res);
   }
 
-  try {
-    const response = await fetch(url, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-      },
-    });
-
-    let text = await response.text();
-
-    res.setHeader("Content-Type", "text/html");
-    res.send(text);
-
-  } catch (e) {
-    res.status(500).send("Proxy error");
-  }
+  res.status(404).send("Not found");
 }
